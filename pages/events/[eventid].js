3f5100435e90f36f417679d729b38d0b5e4c191f
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-05-10 14:44:03 +0100

    add server side rendering to /events/eventid page

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,19 +1,7 @@
-import { useEffect, useState } from "react";
-import { getEventById } from "/helpers/api";
-import { useRouter } from "next/router";
+import { getEventById, getAllEvents } from "/helpers/api";
 
-export default function IndividualEventPage() {
-  const router = useRouter();
-  const { eventid } = router.query;
-  const [event, setEvent] = useState();
-
-  useEffect(() => {
-    getEventById(eventid).then((data) => {
-      setEvent(data);
-    });
-  }, [eventid]);
-
-  if (isNaN(eventid)) {
+export default function IndividualEventPage(props) {
+  if (!props.event || isNaN(props.event.id)) {
     return (
       <>
         <p>Invalid URL</p>
@@ -21,16 +9,39 @@ export default function IndividualEventPage() {
     );
   }
 
-  if (!event) {
+  if (!props.event) {
     return <p>Loading...</p>;
   }
 
   return (
     <div>
-      <h1>{event.title}</h1>
-      <h2>{event.date}</h2>
-      <h3>{event.location}</h3>
-      <p>{event.description}</p>
+      <h1>{props.event.title}</h1>
+      <h2>{props.event.date}</h2>
+      <h3>{props.event.location}</h3>
+      <p>{props.event.description}</p>
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventid;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      event: event || null,
+    },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map((event) => ({ params: { eventid: `${event.id}` } }));
+
+  return {
+    paths: paths,
+    fallback: "blocking",
+  };
 }
