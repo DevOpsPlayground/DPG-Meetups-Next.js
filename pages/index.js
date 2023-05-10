5c39e99379738cbf32062c00410a4c679eccebb3
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-05-10 12:05:03 +0100

    add server rendering using static props

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
+import Head from 'next/head';
 import EventList from "./events";
+import { getAllEvents } from '/helpers/api';
 
-export default function Homepage() {
 
+export default function Homepage(props) {
   return (
     <div className='Homepage'>
-      <EventList />
+      <Head>
+        <title>Devops Playground Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to learn...'
+        />
+      </Head>
+      <EventList events={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 1800,
+  };
 }
