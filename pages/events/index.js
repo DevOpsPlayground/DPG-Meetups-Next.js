e5506edd721251e1e3957c932346a08d4431ccca
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-05-15 13:55:39 +0100

    fix headers

--- pages/events/index.js
+++ pages/events/index.js
@@ -4,6 +4,7 @@ import { getAllEvents } from "/helpers/api";
 import EventList from "/components/events/EventList";
 import EventsSearch from "@/components/events/EventSearch";
 import { MongoClient } from "mongodb";
+import styles from "./../Index.module.css"
 
 function AllEventsPage({ events }) {
   const router = useRouter();
@@ -37,6 +38,7 @@ function AllEventsPage({ events }) {
           content='Find a lot of great events that allow you to evolve...'
         />
       </Head>
+      <h1 className={styles.main_title}>All Events</h1>
       <EventsSearch onSearch={findEventsHandler} />
       <EventList events={events} />
     </>
@@ -46,7 +48,7 @@ function AllEventsPage({ events }) {
 export async function getStaticProps() {
 
   const DB_STRING = `mongodb+srv://${process.env.mongoDB_username}:${process.env.mongoDB_password}@${process.env.mongoDB_cluster}.21rhrh2.mongodb.net/${process.env.mongoDB_database}?retryWrites=true&w=majority`;
-  
+
   const client = await MongoClient.connect(DB_STRING);
   const db = client.db();
   const meetupCollection = db.collection("meetups");
