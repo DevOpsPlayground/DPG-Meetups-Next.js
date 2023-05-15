e3ba4febfd73365ad96dcc3f6fc90eb6aae57953
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-05-15 10:47:15 +0100

    add json file with data

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    mongoDB_username: "", 
+    mongoDB_password: "",
+    mongoDB_cluster: "", 
+    mongoDB_database: "",
+  }
 }
 
 module.exports = nextConfig
